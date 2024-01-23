7103c8316166625e103192b8d6a85e9cc8c9c7ab
Michael Farquhar <michaelfarquhar4@gmail.com>
2024-01-22 20:57:20 -0500

    Add profile intro section and content vars

---
+++ src/content-types.ts
@@ -0,0 +1,9 @@
+export type Title           = string;
+export type Description     = string;
+export type Location        = string;
+export type Gender          = string;
+
+export type ProfileImage    = string;
+export type BannerImage     = string;
+
+export type Theme           = 'light' | 'dark';
---
+++ src/content.ts
@@ -0,0 +1,16 @@
+import type * as T from './content-types';
+
+export const TITLE: T.Title                 = 'My Site';
+export const DESCRIPTION: T.Description     = 'lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam quis nostrud exercitation';
+export const LOCATION: T.Location           = 'Ontario, Canada';
+export const GENDER: T.Gender               = 'Male';
+
+// Profile image is of size 128px x 128px
+export const PROFILE_IMAGE: T.ProfileImage  = 'profile.jpg';
+
+// Can be either a filename or color code
+// Example: banner.jpg or #1D31DB
+export const BANNER_IMAGE: T.BannerImage    = 'banner.jpg';
+
+// Can be either 'light' or 'dark'
+export const THEME: T.Theme                 = 'light';
